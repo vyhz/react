becc084ac7096049eb5e3a68b5e367a1c4b3ac44
Baudenne Rémy <mr.g4ming21200@gmail.com>
2024-03-25 11:04:00 +0100

    debut

---
+++ App.style.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from "react-native";
+
+export const s = StyleSheet.create({
+    square : {
+        backgroundColor : "grey",
+        height: 200,
+        width : "50%"
+    }
+})
